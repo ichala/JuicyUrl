2fdd6309a940f8e4175fa1dae86920c1365760f7
Ali Jendoubi <alijandhd@gmail.com>
2022-12-02 17:32:58 +0100

    Remove console.log

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -9,7 +9,6 @@ class LinksController < ApplicationController
     if current_user.urls.exists?(params[:id])
       @link = current_user.urls.find(params[:id])
       @visits = @link.visits.group('DATE(created_at)').count
-      p @visits
 
     else
       redirect_to links_path, alert: "You don't have access to this link or link doesn't exist"
