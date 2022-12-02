6971b973185f25973a7b1307e56feab512748d47
Ali Jendoubi <alijandhd@gmail.com>
2022-12-02 14:49:32 +0100

    Pin Chartkick to importmaps

--- config/importmap.rb
+++ config/importmap.rb
@@ -4,4 +4,6 @@ pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin "chartkick", to: "chartkick.js", preload: true
+pin "Chart.bundle", to: "Chart.bundle.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
