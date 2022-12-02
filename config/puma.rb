7721c4350b56f6419066cfb5ef1b7f1d5c3c3e4f
Ali Jendoubi <alijandhd@gmail.com>
2022-12-02 16:17:53 +0100

    Update render shell

--- config/puma.rb
+++ config/puma.rb
@@ -30,7 +30,7 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
 #
-# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+ workers ENV.fetch("WEB_CONCURRENCY") { 4 }
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
@@ -39,6 +39,5 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 #
 # preload_app!
 
-workers ENV.fetch("WEB_CONCURRENCY") { 4 }
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
